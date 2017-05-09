f983cb71ed4cc5d587f2516dd729c6178a7444a1
enrevol <enrevol@gmail.com>
2017-05-09 15:38:55 +0700

    Update android.mk.

--- proj.android_studio/src/main/jni/Android.mk
+++ proj.android_studio/src/main/jni/Android.mk
@@ -1,22 +1,47 @@
 LOCAL_PATH := $(call my-dir)
+EE_X_ROOT_DIR = $(LOCAL_PATH)/../../../..
+
+########
+# CORE #
+########
 
 include $(CLEAR_VARS)
 
 LOCAL_CPP_FEATURES += exceptions
 
-LOCAL_MODULE := ee_x_static
+LOCAL_MODULE := ee_x_core_static
 
-LOCAL_MODULE_FILENAME := lib_ee_x
-
-PROJ_DIR = $(LOCAL_PATH)/../../../..
-
-LOCAL_SRC_FILES := ${shell find $(PROJ_DIR)/src/ee -name "*.cpp" -print}
-LOCAL_SRC_FILES += ${shell find $(PROJ_DIR)/proj.android_studio/cpp/ee -name "*.cpp" -print}
+LOCAL_SRC_FILES := ${shell find $(EE_X_ROOT_DIR)/src/ee/core -name "*.cpp" -print}
+LOCAL_SRC_FILES += ${shell find $(EE_X_ROOT_DIR)/proj.android_studio/cpp/ee/core -name "*.cpp" -print}
                    
-LOCAL_C_INCLUDES := $(PROJ_DIR)/src
-LOCAL_C_INCLUDES += $(PROJ_DIR)/proj.android_studio/cpp
+LOCAL_C_INCLUDES := $(EE_X_ROOT_DIR)/src
+LOCAL_C_INCLUDES += $(EE_X_ROOT_DIR)/proj.android_studio/cpp
 
-LOCAL_EXPORT_C_INCLUDES := $(PROJ_DIR)/src
-LOCAL_EXPORT_C_INCLUDES += $(PROJ_DIR)/proj.android_studio/cpp
+LOCAL_EXPORT_C_INCLUDES := $(EE_X_ROOT_DIR)/src
+LOCAL_EXPORT_C_INCLUDES += $(EE_X_ROOT_DIR)/proj.android_studio/cpp
+
+include $(BUILD_STATIC_LIBRARY)
+
+###############
+# CRASHLYTICS #
+###############
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ee_x_crashlytics_static
+LOCAL_SRC_FILES := ${shell find $(EE_X_ROOT_DIR)/src/ee/crashlytics -name "*.cpp" -print}
+LOCAL_STATIC_LIBRARIES := ee_x_core_static
+
+include $(BUILD_STATIC_LIBRARY)
+
+################
+# NOTIFICATION #
+################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ee_x_notification_static
+LOCAL_SRC_FILES := ${shell find $(EE_X_ROOT_DIR)/src/ee/notification -name "*.cpp" -print}
+LOCAL_STATIC_LIBRARIES := ee_x_core_static
 
 include $(BUILD_STATIC_LIBRARY)

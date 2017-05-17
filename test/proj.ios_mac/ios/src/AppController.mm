c9ec63345248ce047fc73d91cb978f9fd68b03c8
enrevol <enrevol@gmail.com>
2017-05-17 11:00:35 +0700

    Update test.

--- test/proj.ios_mac/ios/src/AppController.mm
+++ test/proj.ios_mac/ios/src/AppController.mm
@@ -10,14 +10,33 @@
 
 #import <ee/Core.hpp>
 
+#include "NotificationAgent.hpp"
+
 @implementation AppController
 
 - (BOOL)application:(UIApplication*)application
     didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
 
     [[EEPluginManager getInstance] addPlugin:@"Crashlytics"];
+    [[EEPluginManager getInstance] addPlugin:@"Notification"];
 
     return YES;
+}
+
+- (void)applicationDidEnterBackground:(UIApplication*)application {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    // Invoke
+    // cocos2d::Application::getInstance()->applicationDidEnterBackground();
+
+    NotificationAgent::getInstance()->scheduleAll();
+}
+
+- (void)applicationWillEnterForeground:(UIApplication*)application {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    // Invoke
+    // cocos2d::Application::getInstance()->applicationWillEnterForeground();
+
+    NotificationAgent::getInstance()->unscheduleAll();
 }
 
 @end

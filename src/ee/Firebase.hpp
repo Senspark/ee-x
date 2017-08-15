5225f7bd2da426ff983a2e52ad34f7e400819558
enrevol <enrevol@gmail.com>
2017-08-15 13:58:13 +0700

    Fix pod firebase.

--- src/ee/Firebase.hpp
+++ src/ee/Firebase.hpp
@@ -10,10 +10,25 @@
 #define EE_X_FIREBASE_HPP_
 
 #include "ee/FirebaseFwd.hpp"
+
+#if __has_include("ee/firebase/FirebaseApp.hpp")
 #include "ee/firebase/FirebaseApp.hpp"
+#endif
+
+#if __has_include("ee/firebase/FirebaseAnalytics.hpp")
 #include "ee/firebase/FirebaseAnalytics.hpp"
+#endif
+
+#if __has_include("ee/firebase/FirebaseRemoteConfig.hpp")
 #include "ee/firebase/FirebaseRemoteConfig.hpp"
+#endif
+
+#if __has_include("ee/firebase/FirebaseStorage.hpp")
 #include "ee/firebase/FirebaseStorage.hpp"
+#endif
+
+#if __has_include("ee/firebase/FirebaseMessaging.hpp")
 #include "ee/firebase/FirebaseMessaging.hpp"
+#endif
 
 #endif /* EE_X_FIREBASE_HPP_ */

80b438140ee368872c541335c3bf1e9ed9e28032
enrevol <enrevol@gmail.com>
2017-10-10 11:06:13 +0700

    Refactor test.

--- test/Classes/AppDelegate.cpp
+++ test/Classes/AppDelegate.cpp
@@ -36,86 +36,19 @@ void scheduleOnce(float delay, const std::function<void()>& f) {
 
 ee::FacebookAds* facebookAds_;
 std::shared_ptr<ee::FacebookNativeAd> facebookNativeAd_;
-} // namespace
 
-AppDelegate::AppDelegate() {}
-
-AppDelegate::~AppDelegate() {}
-
-void AppDelegate::initGLContextAttrs() {
-#if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 16, 8};
-#else  // CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
-    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
-#endif // CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
-    cocos2d::GLView::setGLContextAttrs(glContextAttrs);
-}
-
-bool AppDelegate::applicationDidFinishLaunching() {
-    cocos2d::log(__PRETTY_FUNCTION__);
-
-    auto director = cocos2d::Director::getInstance();
-    auto glView = director->getOpenGLView();
-    if (glView == nullptr) {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) ||                               \
-    (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) ||                                 \
-    (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-        glView = cocos2d::GLViewImpl::createWithRect(
-            "HelloCpp", cocos2d::Rect(0, 0, DesignResolution.width,
-                                      DesignResolution.height));
-#else
-        glView = cocos2d::GLViewImpl::create("HelloCpp");
-#endif
-        director->setOpenGLView(glView);
-    }
-
-    director->setDisplayStats(true);
-    director->setAnimationInterval(1.0f / 60);
-
-    auto resolutionPolicy = (DesignResolution.height > DesignResolution.width
-                                 ? ResolutionPolicy::FIXED_WIDTH
-                                 : ResolutionPolicy::FIXED_HEIGHT);
-    glView->setDesignResolutionSize(DesignResolution.width,
-                                    DesignResolution.height, resolutionPolicy);
-    auto&& frameSize = glView->getFrameSize();
-    getLogger().info(cocos2d::StringUtils::format(
-        "frameSize = %f %f", frameSize.width, frameSize.height));
-
-    auto&& winSize = director->getWinSize();
-    getLogger().info(cocos2d::StringUtils::format(
-        "winSize = %f %f", winSize.width, winSize.height));
-
+void testFacebookNativeAd() {
+    auto&& frameSize =
+        cocos2d::Director::getInstance()->getOpenGLView()->getFrameSize();
     int screenWidth = static_cast<int>(frameSize.width);
     int screenHeight = static_cast<int>(frameSize.height);
-
-    ee::Metrics::initialize(frameSize.height / winSize.height);
-    constexpr float points = 1;
-    auto metrics = ee::Metrics::fromPoint(points);
-    auto dp = metrics.toDip();
-    auto pixels = metrics.toPixel();
-    getLogger().info(cocos2d::StringUtils::format("%f pt = %f pixels = %f dp",
-                                                  points, pixels, dp));
-
-    CrashlyticsAgent::getInstance()->initialize();
-    CrashlyticsAgent::getInstance()->logDebug("debug_message");
-    CrashlyticsAgent::getInstance()->logInfo("info_message");
-    CrashlyticsAgent::getInstance()->logError("error_message");
-    CrashlyticsAgent::getInstance()->trackLevelStart("level_test");
-    CrashlyticsAgent::getInstance()->trackLevelEnd("level_test", 100, true);
-    CrashlyticsAgent::getInstance()->trackPurchase(
-        100.f, "USD", true, "Item Test", "Test Type", "TEST_ID");
-
-    CrashlyticsAgent::CustomAttributesType attrs;
-    attrs["title"] = "Beautiful in white";
-    attrs["singer"] = "Westlife";
-    CrashlyticsAgent::getInstance()->trackCustomEvent("PlaySong", attrs);
-    CrashlyticsAgent::getInstance()->trackInvite("Twitter");
 
     float delay = 0.0f;
     scheduleOnce(delay += 1.0f, [] {
         getLogger().info("Create FacebookAds plugin");
         static auto plugin = ee::FacebookAds();
         facebookAds_ = &plugin;
+        facebookAds_->clearTestDevices();
         facebookAds_->addTestDevice(facebookAds_->getTestDeviceHash());
         facebookAds_->addTestDevice(
             "ad45c323f6a9b07f7a9c072549efb279"); // BlueStacks.
@@ -198,6 +131,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
             getLogger().info("Resize = screen size");
             facebookNativeAd_->setPosition(0, 0);
             facebookNativeAd_->setSize(screenWidth, screenHeight);
+            facebookNativeAd_->load();
         });
     });
 
@@ -208,6 +142,78 @@ bool AppDelegate::applicationDidFinishLaunching() {
             facebookNativeAd_->setSize(screenWidth / 2, screenHeight / 2);
         });
     });
+}
+} // namespace
+
+AppDelegate::AppDelegate() {}
+
+AppDelegate::~AppDelegate() {}
+
+void AppDelegate::initGLContextAttrs() {
+#if CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 16, 8};
+#else  // CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+    GLContextAttrs glContextAttrs = {8, 8, 8, 8, 24, 8};
+#endif // CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID
+    cocos2d::GLView::setGLContextAttrs(glContextAttrs);
+}
+
+bool AppDelegate::applicationDidFinishLaunching() {
+    cocos2d::log(__PRETTY_FUNCTION__);
+
+    auto director = cocos2d::Director::getInstance();
+    auto glView = director->getOpenGLView();
+    if (glView == nullptr) {
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) ||                               \
+    (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) ||                                 \
+    (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+        glView = cocos2d::GLViewImpl::createWithRect(
+            "HelloCpp", cocos2d::Rect(0, 0, DesignResolution.width,
+                                      DesignResolution.height));
+#else
+        glView = cocos2d::GLViewImpl::create("HelloCpp");
+#endif
+        director->setOpenGLView(glView);
+    }
+
+    director->setDisplayStats(true);
+    director->setAnimationInterval(1.0f / 60);
+
+    auto resolutionPolicy = (DesignResolution.height > DesignResolution.width
+                                 ? ResolutionPolicy::FIXED_WIDTH
+                                 : ResolutionPolicy::FIXED_HEIGHT);
+    glView->setDesignResolutionSize(DesignResolution.width,
+                                    DesignResolution.height, resolutionPolicy);
+    auto&& frameSize = glView->getFrameSize();
+    getLogger().info(cocos2d::StringUtils::format(
+        "frameSize = %f %f", frameSize.width, frameSize.height));
+
+    auto&& winSize = director->getWinSize();
+    getLogger().info(cocos2d::StringUtils::format(
+        "winSize = %f %f", winSize.width, winSize.height));
+
+    ee::Metrics::initialize(frameSize.height / winSize.height);
+    constexpr float points = 1;
+    auto metrics = ee::Metrics::fromPoint(points);
+    auto dp = metrics.toDip();
+    auto pixels = metrics.toPixel();
+    getLogger().info(cocos2d::StringUtils::format("%f pt = %f pixels = %f dp",
+                                                  points, pixels, dp));
+
+    CrashlyticsAgent::getInstance()->initialize();
+    CrashlyticsAgent::getInstance()->logDebug("debug_message");
+    CrashlyticsAgent::getInstance()->logInfo("info_message");
+    CrashlyticsAgent::getInstance()->logError("error_message");
+    CrashlyticsAgent::getInstance()->trackLevelStart("level_test");
+    CrashlyticsAgent::getInstance()->trackLevelEnd("level_test", 100, true);
+    CrashlyticsAgent::getInstance()->trackPurchase(
+        100.f, "USD", true, "Item Test", "Test Type", "TEST_ID");
+
+    CrashlyticsAgent::CustomAttributesType attrs;
+    attrs["title"] = "Beautiful in white";
+    attrs["singer"] = "Westlife";
+    CrashlyticsAgent::getInstance()->trackCustomEvent("PlaySong", attrs);
+    CrashlyticsAgent::getInstance()->trackInvite("Twitter");
 
     cocos2d::log("Create scene");
 

09a94935d7be854f44e612222b663320a42a875d
enrevol <enrevol@gmail.com>
2017-10-18 15:47:51 +0700

    Fix include.

--- src/ee/Ads.hpp
+++ src/ee/Ads.hpp
@@ -11,6 +11,8 @@
 
 #include "ee/ads/AdViewInterface.hpp"
 #include "ee/ads/InterstitialAdInterface.hpp"
+#include "ee/ads/MultiAdView.hpp"
+#include "ee/ads/MultiInterstitialAd.hpp"
 #include "ee/ads/MultiRewardedVideo.hpp"
 #include "ee/ads/RewardedVideoInterface.hpp"
 

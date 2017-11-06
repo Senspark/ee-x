2dad34e2a2953ccf472983c168ef31d655b4f171
enrevol <enrevol@gmail.com>
2017-11-06 16:20:03 +0700

    Default native ad size is screen size / 4 (test project).

--- test/Classes/MultiNativeAdTestScene.cpp
+++ test/Classes/MultiNativeAdTestScene.cpp
@@ -13,15 +13,22 @@
 #include <ee/Ads.hpp>
 #include <ee/Core.hpp>
 
+#include <cocos2d.h>
+
 namespace eetest {
 NativeAdTestScene* createMultiNativeAdTestScene() {
+    auto&& frameSize =
+        cocos2d::Director::getInstance()->getOpenGLView()->getFrameSize();
+    int screenWidth = static_cast<int>(frameSize.width);
+    int screenHeight = static_cast<int>(frameSize.height);
+
     auto adView =
         ee::runOnUiThreadAndWaitResult<std::shared_ptr<ee::AdViewInterface>>(
-            [] {
+            [screenWidth, screenHeight] {
                 auto ad = std::make_shared<ee::MultiAdView>();
                 ad->addItem(createAdMobNativeAd());
                 ad->addItem(createFacebookNativeAd());
-                ad->setSize(100, 100);
+                ad->setSize(screenWidth / 4, screenHeight / 4);
                 return ad;
             });
     return NativeAdTestScene::createWithAdView(adView);

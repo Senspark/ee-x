468fc24c3851e756fff5242158dcb0f6d9a4671c
enrevol <enrevol@gmail.com>
2017-11-09 14:23:07 +0700

    Update store iOS.

--- src/ee/store/internal/EEStore.m
+++ src/ee/store/internal/EEStore.m
@@ -8,14 +8,39 @@
 
 #import <StoreKit/StoreKit.h>
 
-#import "ee/store/internal/EEStore.h"
-#import "ee/core/internal/EEMessageBridge.h"
 #import "ee/core/internal/EEDictionaryUtils.h"
 #import "ee/core/internal/EEJsonUtils.h"
+#import "ee/core/internal/EEMessageBridge.h"
+#import "ee/store/internal/EEStore.h"
+
+@interface EEStore () <SKProductsRequestDelegate, SKPaymentTransactionObserver>
+
+@end
 
 @implementation EEStore
 
-+ (instancetype)getInstance {
+// clang-format off
+static NSString* const k__request_products_succeeded  = @"Store_requestProductSucceeded";
+static NSString* const k__request_products_failed     = @"Store_requestProductFailed";
+static NSString* const k__restore_purchases_succeeded = @"Store_restorePurchasesSucceeded";
+static NSString* const k__restore_purchases_failed    = @"Store_restorePurchasesFailed";
+static NSString* const k__transaction_succeeded       = @"Store_transactionSucceeded";
+static NSString* const k__transaction_failed          = @"Store_transactionFailed";
+static NSString* const k__transaction_restored        = @"Store_transactionRestored";
+// clang-format on
+
+// clang-format off
+static NSString* const k__title             = @"title";
+static NSString* const k__description       = @"description";
+static NSString* const k__price             = @"price";
+static NSString* const k__product_id        = @"product_id";
+static NSString* const k__transaction_id    = @"transaction_id";
+static NSString* const k__currency_symbol   = @"currency_symbol";
+static NSString* const k__currency_code     = @"currency_code";
+static NSString* const k__error_code        = @"error_code";
+// clang-format on
+
++ (instancetype _Nullable)getInstance {
     static EEStore* sharedInstance;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
@@ -33,38 +58,49 @@
     if (self == nil) {
         return nil;
     }
-    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue addTransactionObserver:self];
     return self;
 }
 
 - (BOOL)initialize {
-
     return YES;
 }
 
 - (void)dealloc {
-    [[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue removeTransactionObserver:self];
     [super dealloc];
 }
 
+- (void)registerHandlers {
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+}
+
+- (void)deregisterHandlers {
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+}
+
 - (void)retryUnfinishedTransactions {
-    NSArray* transactions = [[SKPaymentQueue defaultQueue] transactions];
-    [self paymentQueue:[SKPaymentQueue defaultQueue]
-        updatedTransactions:transactions];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    NSArray* transactions = [paymentQueue transactions];
+    [self paymentQueue:paymentQueue updatedTransactions:transactions];
 }
 
 - (void)restoreTransactions {
-    [[SKPaymentQueue defaultQueue] restoreCompletedTransactions];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue restoreCompletedTransactions];
 }
 
-- (void)addPayment:(NSString*)productIdentifier {
+- (void)addPayment:(NSString* _Nonnull)productIdentifier {
     SKMutablePayment* payment = [[[SKMutablePayment alloc] init] autorelease];
     [payment setProductIdentifier:productIdentifier];
     [payment setQuantity:1];
-    [[SKPaymentQueue defaultQueue] addPayment:payment];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue addPayment:payment];
 }
 
-- (void)requestProducts:(NSSet*)identifiers {
+- (void)requestProducts:(NSSet* _Nonnull)identifiers {
     SKProductsRequest* request = [[[SKProductsRequest alloc]
         initWithProductIdentifiers:identifiers] autorelease];
     [request setDelegate:self];
@@ -73,7 +109,8 @@
 
 - (void)productsRequest:(SKProductsRequest*)request
      didReceiveResponse:(SKProductsResponse*)response {
-    NSMutableArray* arr = [NSMutableArray alloc];
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    NSMutableArray* arr = [NSMutableArray array];
 
     NSArray* products = [response products];
     for (SKProduct* product in products) {
@@ -84,26 +121,26 @@
         NSString* productId = [product productIdentifier];
 
         NSDictionary* dict = @{
-            @"title": title,
-            @"description": description,
-            @"price": @([price doubleValue]),
-            @"productId": productId,
-            @"currencySymbol": [locale objectForKey:NSLocaleCurrencySymbol],
-            @"currencyCode": [locale objectForKey:NSLocaleCurrencyCode]
+            k__title: title,
+            k__description: description,
+            k__price: @([price doubleValue]),
+            k__product_id: productId,
+            k__currency_symbol: [locale objectForKey:NSLocaleCurrencySymbol],
+            k__currency_code: [locale objectForKey:NSLocaleCurrencyCode]
         };
         [arr addObject:dict];
     }
 
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_products_request_succeeded"
-            msg:[EEJsonUtils convertArrayToString:arr]];
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__request_products_succeeded
+            message:[EEJsonUtils convertArrayToString:arr]];
 }
 
 - (void)request:(SKRequest*)request didFailWithError:(NSError*)error {
-    NSDictionary* dict = [EEDictionaryUtils createDictWithError:error];
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_products_request_failed"
-            msg:[EEJsonUtils convertDictionaryToString:dict]];
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, [error localizedDescription]);
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__request_products_failed
+            message:[@([error code]) stringValue]];
 }
 
 - (void)paymentQueue:(SKPaymentQueue*)queue
@@ -136,76 +173,88 @@
 
 - (void)paymentQueue:(SKPaymentQueue*)queue
     restoreCompletedTransactionsFailedWithError:(NSError*)error {
-    NSDictionary* dict = [EEDictionaryUtils createDictWithError:error];
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_restore_transactions_failed"
-            msg:[EEJsonUtils convertDictionaryToString:dict]];
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, [error localizedDescription]);
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__restore_purchases_failed
+            message:[error localizedDescription]];
 }
 
 - (void)paymentQueueRestoreCompletedTransactionsFinished:
-        (SKPaymentQueue*)queue {
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_restore_transactions_succeeded"
-            msg:@""];
+    (SKPaymentQueue*)queue {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__restore_purchases_succeeded];
 }
 
 - (void)purchaseTransaction:(SKPaymentTransaction*)transaction {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
     NSAssert([transaction transactionState] ==
                  SKPaymentTransactionStatePurchased,
-             @"..");
+             @"Unexpected transaction state");
 
-    NSString* productIdentifier = [[transaction payment] productIdentifier];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue finishTransaction:transaction];
+
+    SKPayment* payment = [transaction payment];
+    NSString* productId = [payment productIdentifier];
+    NSString* transactionId = [transaction transactionIdentifier];
     NSDictionary* dict = @{
-        @"productIdentifier": productIdentifier,
-        @"transactionIdentifier": [transaction transactionIdentifier],
+        k__product_id: productId,
+        k__transaction_id: transactionId,
     };
 
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_transaction_purchased"
-            msg:[EEJsonUtils convertDictionaryToString:dict]];
-
-    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__transaction_succeeded
+            message:[EEJsonUtils convertDictionaryToString:dict]];
 }
 
 - (void)failTransaction:(SKPaymentTransaction*)transaction {
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__,
+          [[transaction error] localizedDescription]);
     NSAssert([transaction transactionState] == SKPaymentTransactionStateFailed,
-             @"..");
+             @"Unexpected transaction state");
 
-    NSMutableDictionary* dict = [NSMutableDictionary dictionary];
-    [EEDictionaryUtils setError:[transaction error] forDict:dict];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue finishTransaction:transaction];
 
-    NSString* productIdentifier = [[transaction payment] productIdentifier];
-    [dict setObject:productIdentifier forKey:@"productIdentifier"];
+    SKPayment* payment = [transaction payment];
+    NSString* productId = [payment productIdentifier];
+    NSDictionary* dict = @{
+        k__product_id: productId,
+        k__error_code: @([[transaction error] code])
+    };
 
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_transaction_failed"
-            msg:[EEJsonUtils convertDictionaryToString:dict]];
-
-    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__transaction_failed
+            message:[EEJsonUtils convertDictionaryToString:dict]];
 }
 
 - (void)restoreTransaction:(SKPaymentTransaction*)transaction {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
     NSAssert([transaction transactionState] ==
                  SKPaymentTransactionStateRestored,
-             @"..");
+             @"Unexpected transaction state");
 
-    NSString* productIdentifier = [[transaction payment] productIdentifier];
+    SKPaymentQueue* paymentQueue = [SKPaymentQueue defaultQueue];
+    [paymentQueue finishTransaction:transaction];
+
+    SKPayment* payment = [transaction payment];
+    NSString* productId = [payment productIdentifier];
+    NSString* transactionId = [transaction transactionIdentifier];
     NSDictionary* dict = @{
-        @"productIdentifier": productIdentifier,
-        @"transactionIdentifier": [transaction transactionIdentifier],
+        k__product_id: productId,
+        k__transaction_id: transactionId,
     };
 
-    [[EEMessageBridge getInstance]
-        callCpp:@"__store_transaction_restored"
-            msg:[EEJsonUtils convertDictionaryToString:dict]];
-
-    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+    EEMessageBridge* bridge = [EEMessageBridge getInstance];
+    [bridge callCpp:k__transaction_restored
+            message:[EEJsonUtils convertDictionaryToString:dict]];
 }
 
 - (void)deferTransaction:(SKPaymentTransaction*)transaction {
     NSAssert([transaction transactionState] ==
                  SKPaymentTransactionStateDeferred,
-             @"..");
+             @"Unexpected transaction state");
     // http://stackoverflow.com/questions/26187148/deferred-transactions
     BOOL fix_clang_format __unused;
 }

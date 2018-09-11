ffaa647f50a093a09e8e05b52046c8553375daff
Le Van Kiet <kiet@senspark.com>
2018-09-11 11:21:36 +0700

    -Adding more methods for firebase analytics.

--- src/ee/firebase/FirebaseAnalytics.cpp
+++ src/ee/firebase/FirebaseAnalytics.cpp
@@ -11,97 +11,164 @@
 
 #if defined(EE_X_MOBILE)
 #include <firebase/analytics.h>
+#include <firebase/analytics/parameter_names.h>
 #endif // EE_X_MOBILE
 
 namespace ee {
-namespace firebase {
-using Self = Analytics;
+    namespace firebase {
+        using Self = Analytics;
 
-Self::Analytics() {
-    initialized_ = false;
-}
+        Self::Analytics() {
+            initialized_ = false;
+        }
 
-Self::~Analytics() {
-    if (initialized_) {
+        Self::~Analytics() {
+            if (initialized_) {
 #if defined(EE_X_MOBILE)
-        ::firebase::analytics::Terminate();
+                ::firebase::analytics::Terminate();
 #endif // EE_X_MOBILE
-    }
-}
+            }
+        }
 
-bool Self::initialize() {
-    if (initialized_) {
-        return true;
-    }
+        bool Self::initialize() {
+            if (initialized_) {
+                return true;
+            }
 
-    App::initialize();
+            App::initialize();
 
 #if defined(EE_X_MOBILE)
-    auto app = ::firebase::App::GetInstance();
-    if (app == nullptr) {
-        return false;
-    }
+            auto app = ::firebase::App::GetInstance();
+            if (app == nullptr) {
+                return false;
+            }
 
-    ::firebase::analytics::Initialize(*app);
-    analyticsCollectionEnabled(true);
-    setMinimumSessionDuration(10000);
-    setSessionTimeoutDuration(1800000);
+            ::firebase::analytics::Initialize(*app);
+            analyticsCollectionEnabled(true);
+            setMinimumSessionDuration(10000);
+            setSessionTimeoutDuration(1800000);
 #endif // defined(EE_X_MOBILE)
 
-    initialized_ = true;
-    return true;
-}
+            initialized_ = true;
+            return true;
+        }
 
-void Self::analyticsCollectionEnabled(bool enabled) {
-    if (not initialized_) {
-        return;
-    }
+        void Self::analyticsCollectionEnabled(bool enabled) {
+            if (not initialized_) {
+                return;
+            }
 #ifdef EE_X_MOBILE
-    ::firebase::analytics::SetAnalyticsCollectionEnabled(enabled);
+            ::firebase::analytics::SetAnalyticsCollectionEnabled(enabled);
 #endif // EE_X_MOBILE
-}
+        }
 
-void Self::setMinimumSessionDuration(std::int64_t milliseconds) {
-    if (not initialized_) {
-        return;
-    }
+        void Self::setMinimumSessionDuration(std::int64_t milliseconds) {
+            if (not initialized_) {
+                return;
+            }
 #ifdef EE_X_MOBILE
-    ::firebase::analytics::SetMinimumSessionDuration(milliseconds);
+            ::firebase::analytics::SetMinimumSessionDuration(milliseconds);
 #endif // EE_X_MOBILE
-}
+        }
 
-void Self::setSessionTimeoutDuration(std::int64_t milliseconds) {
-    if (not initialized_) {
-        return;
-    }
+        void Self::setSessionTimeoutDuration(std::int64_t milliseconds) {
+            if (not initialized_) {
+                return;
+            }
 #ifdef EE_X_MOBILE
-    ::firebase::analytics::SetSessionTimeoutDuration(milliseconds);
+            ::firebase::analytics::SetSessionTimeoutDuration(milliseconds);
 #endif // EE_X_MOBILE
-}
+        }
 
-void Self::setUserId(const std::string& userId) {
-    if (not initialized_) {
-        return;
-    }
+        void Self::setUserId(const std::string& userId) {
+            if (not initialized_) {
+                return;
+            }
 #ifdef EE_X_MOBILE
-    ::firebase::analytics::SetUserId(userId.c_str());
+            ::firebase::analytics::SetUserId(userId.c_str());
 #endif // EE_X_MOBILE
-}
+        }
 
-void Self::setUserProperty(const std::string& name,
-                           const std::string& property) {
-    if (not initialized_) {
-        return;
-    }
+        void Self::setUserProperty(const std::string& name,
+                                   const std::string& property) {
+            if (not initialized_) {
+                return;
+            }
 #if defined(EE_X_MOBILE)
-    ::firebase::analytics::SetUserProperty(name.c_str(), property.c_str());
+            ::firebase::analytics::SetUserProperty(name.c_str(), property.c_str());
 #endif // EE_X_MOBILE
-}
+        }
 
-void Self::logEvent(const std::string& name) {
+        void Self::logEvent(const std::string& name) {
 #ifdef EE_X_MOBILE
-    ::firebase::analytics::LogEvent(name.c_str());
+            ::firebase::analytics::LogEvent(name.c_str());
 #endif // EE_X_MOBILE
-}
-} // namespace firebase
+        }
+
+        void Self::logScreen(const std::string& screenName) const {
+#ifdef EE_X_MOBILE
+            ::firebase::analytics::LogEvent("screen",
+                                            ::firebase::analytics::kParameterItemName,
+                                            screenName.c_str());
+#endif // EE_X_MOBILE
+        }
+
+        void Self::logCustomEvent(const std::string& category,
+                                  const std::string& action, const std::string& label,
+                                  float value) const {
+#ifdef EE_X_MOBILE
+            ::firebase::analytics::Parameter parameters[] = {
+                    ::firebase::analytics::Parameter("category", category),
+                    ::firebase::analytics::Parameter("action", action),
+                    ::firebase::analytics::Parameter("label", label),
+                    ::firebase::analytics::Parameter("value", value),
+            };
+            ::firebase::analytics::LogEvent("event", parameters,
+                                            sizeof(parameters) / sizeof(parameters[0]));
+#endif // EE_X_MOBILE
+        }
+
+        void Self::logEcommerce(const std::string& productId,
+                                const std::string& productName,
+                                const std::string& category, float price,
+                                bool success) const {
+#ifdef EE_X_MOBILE
+            ::firebase::analytics::Parameter parameters[] = {
+                    ::firebase::analytics::Parameter(
+                            ::firebase::analytics::kParameterItemID, productId),
+                    ::firebase::analytics::Parameter(
+                            ::firebase::analytics::kParameterItemName, productName),
+                    ::firebase::analytics::Parameter(
+                            ::firebase::analytics::kParameterItemCategory, category),
+                    ::firebase::analytics::Parameter(::firebase::analytics::kParameterPrice,
+                                                     price),
+                    ::firebase::analytics::Parameter(
+                            ::firebase::analytics::kParameterSuccess, success),
+            };
+            ::firebase::analytics::LogEvent("buy", parameters,
+                                            sizeof(parameters) / sizeof(parameters[0]));
+#endif // EE_X_MOBILE
+        }
+
+        void Self::logGameStart(const std::string& type,
+                                const std::string& difficulty) const {
+            ::firebase::analytics::Parameter parameters[] = {
+                    ::firebase::analytics::Parameter("type", type),
+                    ::firebase::analytics::Parameter("difficulty", difficulty),
+            };
+            ::firebase::analytics::LogEvent("game_start", parameters,
+                                            sizeof(parameters) / sizeof(parameters[0]));
+        }
+
+        void Self::logGameEnd(const std::string& type, const std::string& difficulty,
+                              int rank) const {
+            ::firebase::analytics::Parameter parameters[] = {
+                    ::firebase::analytics::Parameter("type", type),
+                    ::firebase::analytics::Parameter("difficulty", difficulty),
+                    ::firebase::analytics::Parameter("rank", rank),
+            };
+            ::firebase::analytics::LogEvent("game_end", parameters,
+                                            sizeof(parameters) / sizeof(parameters[0]));
+        }
+    } // namespace firebase
 } // namespace ee
--- src/ee/firebase/FirebaseAnalytics.hpp
+++ src/ee/firebase/FirebaseAnalytics.hpp
@@ -13,66 +13,81 @@
 #include <string>
 
 namespace ee {
-namespace firebase {
+    namespace firebase {
 /// Wrapper for Firebase Analytics.
 /// Should have a single instance at a time.
 /// https://firebase.google.com/docs/analytics/cpp/start
 /// https://firebase.google.com/docs/reference/cpp/namespace/firebase/analytics
-class Analytics {
-public:
-    Analytics();
-    ~Analytics();
+        class Analytics {
+        public:
+            Analytics();
+            ~Analytics();
 
-    /// Initializes the firebase analytics.
-    /// @return True if successfully initialized, false otherwise.
-    bool initialize();
+            /// Initializes the firebase analytics.
+            /// @return True if successfully initialized, false otherwise.
+            bool initialize();
 
-    /// Sets whether analytics collection is enabled for this app on this
-    /// device.
-    /// This setting is persisted across app sessions. By default it is enabled.
-    /// @param[in] enabled true to enable analytics collection, false to
-    /// disable.
-    void analyticsCollectionEnabled(bool enabled);
+            /// Sets whether analytics collection is enabled for this app on this
+            /// device.
+            /// This setting is persisted across app sessions. By default it is enabled.
+            /// @param[in] enabled true to enable analytics collection, false to
+            /// disable.
+            void analyticsCollectionEnabled(bool enabled);
 
-    /// Sets the minimum engagement time required before starting a session.
-    /// @note The default value is 10000 (10 seconds).
-    /// @param[in] milliseconds The minimum engagement time required to start a
-    /// new session.
-    void setMinimumSessionDuration(std::int64_t milliseconds);
+            /// Sets the minimum engagement time required before starting a session.
+            /// @note The default value is 10000 (10 seconds).
+            /// @param[in] milliseconds The minimum engagement time required to start a
+            /// new session.
+            void setMinimumSessionDuration(std::int64_t milliseconds);
 
-    /// @brief Sets the duration of inactivity that terminates the current
-    /// session.
-    ///
-    /// @note The default value is 1800000 (30 minutes).
-    ///
-    /// @param milliseconds The duration of inactivity that terminates the
-    /// current session.
-    void setSessionTimeoutDuration(std::int64_t milliseconds);
+            /// @brief Sets the duration of inactivity that terminates the current
+            /// session.
+            ///
+            /// @note The default value is 1800000 (30 minutes).
+            ///
+            /// @param milliseconds The duration of inactivity that terminates the
+            /// current session.
+            void setSessionTimeoutDuration(std::int64_t milliseconds);
 
-    /// Sets the user ID property.
-    /// This feature must be used in accordance with
-    /// <a href="https://www.google.com/policies/privacy">Google's Privacy
-    /// Policy</a>
-    /// @param[in] userId The user ID associated with the user of this app on
-    /// this device. The user ID must be non-empty and no more than 36
-    /// characters long. Setting user_id to NULL or nullptr removes the user ID.
-    void setUserId(const std::string& userId);
+            /// Sets the user ID property.
+            /// This feature must be used in accordance with
+            /// <a href="https://www.google.com/policies/privacy">Google's Privacy
+            /// Policy</a>
+            /// @param[in] userId The user ID associated with the user of this app on
+            /// this device. The user ID must be non-empty and no more than 36
+            /// characters long. Setting user_id to NULL or nullptr removes the user ID.
+            void setUserId(const std::string& userId);
 
-    /// Set a user property to the given value.
-    /// @param[in] name Name of the user property to set.
-    /// @param[in] property Value to set the user property to.
-    void setUserProperty(const std::string& name, const std::string& property);
+            /// Set a user property to the given value.
+            /// @param[in] name Name of the user property to set.
+            /// @param[in] property Value to set the user property to.
+            void setUserProperty(const std::string& name, const std::string& property);
 
-    /// Log an event with no parameters.
-    /// @param[in] name Name of the event to log. Should contain 1 to 32
-    /// alphanumeric characters or underscores. The name must start with an
-    /// alphabetic character. Some event names are reserved.
-    void logEvent(const std::string& name);
+            /// Log an event with no parameters.
+            /// @param[in] name Name of the event to log. Should contain 1 to 32
+            /// alphanumeric characters or underscores. The name must start with an
+            /// alphabetic character. Some event names are reserved.
+            void logEvent(const std::string& name);
 
-private:
-    bool initialized_;
-};
-} // namespace firebase
+            void logScreen(const std::string& screenName) const;
+
+            void logCustomEvent(const std::string& category, const std::string& action,
+                                const std::string& label, float value) const;
+            void logEcommerce(const std::string& productId,
+                              const std::string& productName,
+                              const std::string& category, float price,
+                              bool success = true) const;
+
+            void logGameStart(const std::string& type,
+                              const std::string& difficulty) const;
+
+            void logGameEnd(const std::string& type, const std::string& difficulty,
+                            int rank) const;
+
+        private:
+            bool initialized_;
+        };
+    } // namespace firebase
 } // namespace ee
 
 #endif /* EE_X_FIREBASE_ANALYTICS_HPP_ */

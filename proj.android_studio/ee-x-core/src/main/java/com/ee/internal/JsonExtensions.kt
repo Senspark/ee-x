a4e13e24f6d4fe561d6befd23fd730cb47369e66
eps <enrevol@gmail.com>
2020-07-06 18:10:23 +0700

    Fix serialize / deserialize map.

--- proj.android_studio/ee-x-core/src/main/java/com/ee/internal/JsonExtensions.kt
+++ proj.android_studio/ee-x-core/src/main/java/com/ee/internal/JsonExtensions.kt
@@ -3,19 +3,30 @@ package com.ee.internal
 import androidx.annotation.AnyThread
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.UnstableDefault
+import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.serializer
 
 @AnyThread
-@UnstableDefault
 @ImplicitReflectionSerializer
+@UnstableDefault
 inline fun <reified T : Any> deserialize(string: String): T {
     return Json.parse(T::class.serializer(), string)
 }
 
 @AnyThread
-@UnstableDefault
 @ImplicitReflectionSerializer
+@UnstableDefault
+inline fun <reified K : Any, reified V : Any> deserializeMap(string: String): Map<K, V> {
+    val keySerializer = K::class.serializer()
+    val valueSerializer = V::class.serializer()
+    val serializer = MapSerializer(keySerializer, valueSerializer)
+    return Json.parse(serializer, string)
+}
+
+@AnyThread
+@ImplicitReflectionSerializer
+@UnstableDefault
 inline fun <reified T : Any> T.serialize(): String {
     return Json.stringify(T::class.serializer(), this)
 }

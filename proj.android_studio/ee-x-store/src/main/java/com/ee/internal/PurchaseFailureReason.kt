689d27cacc6160b0fe0fde350dd05f2818119643
Tu Duc Hoang <hoangtd@senspark.com>
2020-07-13 18:39:13 +0700

    Fix serializing/deserializing enum classes.

--- proj.android_studio/ee-x-store/src/main/java/com/ee/internal/PurchaseFailureReason.kt
+++ proj.android_studio/ee-x-store/src/main/java/com/ee/internal/PurchaseFailureReason.kt
@@ -1,5 +1,15 @@
 package com.ee.internal
 
+import kotlinx.serialization.Decoder
+import kotlinx.serialization.Encoder
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.PrimitiveDescriptor
+import kotlinx.serialization.PrimitiveKind
+import kotlinx.serialization.SerialDescriptor
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+
+@Serializable
 enum class PurchaseFailureReason {
     BillingUnavailable,
     ExistingPurchasePending,
@@ -8,5 +18,19 @@ enum class PurchaseFailureReason {
     UserCancelled,
     PaymentDeclined,
     DuplicateTransaction,
-    Unknown,
+    Unknown;
+
+    @Serializer(forClass = PurchaseFailureReason::class)
+    companion object : KSerializer<PurchaseFailureReason> {
+        override val descriptor: SerialDescriptor
+            get() = PrimitiveDescriptor("com.ee.internal.PurchaseFailureReason", PrimitiveKind.INT)
+
+        override fun serialize(encoder: Encoder, value: PurchaseFailureReason) {
+            encoder.encodeInt(value.ordinal)
+        }
+
+        override fun deserialize(decoder: Decoder): PurchaseFailureReason {
+            return PurchaseFailureReason.values()[decoder.decodeInt()]
+        }
+    }
 }

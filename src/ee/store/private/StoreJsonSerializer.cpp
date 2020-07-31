73ea6755ea1d910c6bb4fd4dfdeda6cca0792524
eps <enrevol@gmail.com>
2020-07-31 14:20:22 +0700

    Fix crash when parsing failure reason (iOS).

--- src/ee/store/private/StoreJsonSerializer.cpp
+++ src/ee/store/private/StoreJsonSerializer.cpp
@@ -159,19 +159,32 @@ Self::deserializeProductDetails(const std::string& json) {
 PurchaseFailureDescription
 Self::deserializeFailureReason(const std::string& json) {
     using Enum = PurchaseFailureReason;
-    static std::map<int, Enum> intToEnum{
-        {0, Enum::PurchasingUnavailable},
-        {1, Enum::ExistingPurchasePending},
-        {2, Enum::ProductUnavailable},
-        {3, Enum::SignatureInvalid},
-        {4, Enum::UserCancelled},
-        {5, Enum::PaymentDeclined},
-        {6, Enum::DuplicateTransaction}};
+
+#if defined(EE_X_ANDROID)
+    static std::map<int, Enum> reasonMap     //
+        {{0, Enum::PurchasingUnavailable},   //
+         {1, Enum::ExistingPurchasePending}, //
+         {2, Enum::ProductUnavailable},      //
+         {3, Enum::SignatureInvalid},        //
+         {4, Enum::UserCancelled},           //
+         {5, Enum::PaymentDeclined},         //
+         {6, Enum::DuplicateTransaction}};
+#endif // defined(EE_X_ANDROID)
+#if defined(EE_X_IOS)
+    static std::map<std::string, Enum> reasonMap{
+        {"PurchasingUnavailable", Enum::PurchasingUnavailable},
+        {"ExistingPurchasePending", Enum::ExistingPurchasePending},
+        {"ProductUnavailable", Enum::ProductUnavailable},
+        {"SignatureInvalid", Enum::SignatureInvalid},
+        {"UserCancelled", Enum::UserCancelled},
+        {"PaymentDeclined", Enum::PaymentDeclined},
+        {"DuplicateTransaction", Enum::DuplicateTransaction}};
+#endif // defined(EE_X_IOS)
 
     auto dic = nlohmann::json::parse(json);
     auto reason = PurchaseFailureReason::Unknown;
-    auto iter = intToEnum.find(dic["reason"]);
-    if (iter != intToEnum.cend()) {
+    auto iter = reasonMap.find(dic["reason"]);
+    if (iter != reasonMap.cend()) {
         reason = iter->second;
     }
     return PurchaseFailureDescription(dic["productId"], reason, dic["message"]);

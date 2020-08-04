9263e81bcc49e1602a4b5e20f6915000201405da
eps <enrevol@gmail.com>
2020-08-04 15:04:23 +0700

    Auto update podfile.

--- unity/Editor/LibrarySettings.cs
+++ unity/Editor/LibrarySettings.cs
@@ -1,5 +1,8 @@
 #if UNITY_EDITOR
 
+using System.IO;
+using System.Xml.Linq;
+
 using UnityEditor;
 
 using UnityEngine;
@@ -9,6 +12,9 @@ namespace EE.Editor {
         private const string LibrarySettingsDir = "Assets/EE";
         private const string LibrarySettingsResDir = "Assets/EE/Resources";
         private const string LibrarySettingsFile = "Assets/EE/Resources/EESettings.asset";
+
+        private const string LibrarySettingsEditorDir = "Assets/EE/Editor";
+        private const string LibraryDependenciesFile = "Assets/EE/Editor/Dependencies.xml";
 
         private static LibrarySettings _sharedInstance;
 
@@ -48,7 +54,42 @@ namespace EE.Editor {
         }
 
         public void WriteSettingsToFile() {
+            UpdateDependencies();
             AssetDatabase.SaveAssets();
+        }
+
+        private void UpdateDependencies() {
+            if (!AssetDatabase.IsValidFolder(LibrarySettingsEditorDir)) {
+                AssetDatabase.CreateFolder(LibrarySettingsDir, "Editor");
+            }
+            XDocument document;
+            try {
+                document = XDocument.Load(LibraryDependenciesFile);
+            } catch (IOException exception) {
+                document = new XDocument();
+            }
+            var dependencies = document.Element("dependencies");
+            if (dependencies == null) {
+                dependencies = new XElement("dependencies");
+                document.Add(dependencies);
+            }
+            var iosPods = dependencies.Element("iosPods");
+            if (iosPods == null) {
+                iosPods = new XElement("iosPods");
+                dependencies.Add(iosPods);
+            }
+            iosPods.RemoveAll();
+            if (IsCoreEnabled) {
+                var iosPod = new XElement("iosPod");
+                iosPod.Add(new XAttribute("name", "ee-x/core"));
+                iosPods.Add(iosPod);
+            }
+            if (IsAdMobEnabled) {
+                var iosPod = new XElement("iosPod");
+                iosPod.Add(new XAttribute("name", "ee-x/admob"));
+                iosPods.Add(iosPod);
+            }
+            document.Save(LibraryDependenciesFile);
         }
     }
 }

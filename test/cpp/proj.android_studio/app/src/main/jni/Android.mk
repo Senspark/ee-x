9e2e9af90f9de8e47bdec515691d87c7ba09f6c2
eps <enrevol@gmail.com>
2020-12-14 22:32:33 +0700

    Fix buid ndk-build.

--- test/cpp/proj.android_studio/app/src/main/jni/Android.mk
+++ test/cpp/proj.android_studio/app/src/main/jni/Android.mk
@@ -15,6 +15,7 @@ LOCAL_SRC_FILES += ${shell find $(PROJECT_DIR)/Classes -name "*.cpp" -print}
 LOCAL_C_INCLUDES := ${shell find $(PROJECT_DIR)/Classes -type d -print}
 
 LOCAL_STATIC_LIBRARIES := cc_static
+LOCAL_STATIC_LIBRARIES += ccui
 LOCAL_STATIC_LIBRARIES += soomla_store
 LOCAL_STATIC_LIBRARIES += ee_x
 LOCAL_STATIC_LIBRARIES += ee_x_firebase_analytics
@@ -26,4 +27,4 @@ LOCAL_STATIC_LIBRARIES += ee_x_firebase_storage
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module, cocos2d/cocos) # Must be placed before ../jni so modules-get-list can work properly.
-$(call import-module, ..)
+$(call import-module, ../..)

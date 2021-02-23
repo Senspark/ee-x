8b8ccfa8b2ce7d623d87d1a3c94bf220b9c03291
eps <enrevol@gmail.com>
2021-02-23 10:21:53 +0700

    Update SKAdNetworkItems list.

--- src/unity/Editor/IosProcessor.cs
+++ src/unity/Editor/IosProcessor.cs
@@ -1,4 +1,5 @@
 #if UNITY_IOS
+using System.Collections.Generic;
 using System.IO;
 
 using UnityEditor.Build;
@@ -24,32 +25,33 @@ namespace EE.Editor {
                     plist.root.SetString("GADApplicationIdentifier", appId);
                 }
             }
-            if (settings.IsAdMobEnabled ||
-                settings.IsIronSourceMediationEnabled) {
-                var items = plist.root["SKAdNetworkItems"]?.AsArray() ?? plist.root.CreateArray("SKAdNetworkItems");
-                items.AddDict().SetString("SKAdNetworkIdentifier", "cstr6suwn9.skadnetwork");
+
+            // SkAdNetwork
+            // https://www.skanids.com/
+            // https://developers.ironsrc.com/ironsource-mobile/ios/ios-14-network-support
+            var networks = new List<string> {
+                "4fzdc2evr5.skadnetwork", "4pfyvq9l8r.skadnetwork", "ydx93a7ass.skadnetwork", "488r3q3dtq.skadnetwork",
+                "6xzpu9s2p8.skadnetwork", "mlmmfzh3r3.skadnetwork", "lr83yxwka7.skadnetwork", "c6k4g5qg8m.skadnetwork",
+                "wg4vff78zm.skadnetwork", "238da6jt44.skadnetwork", "22mmun2rn5.skadnetwork", "3sh42y64q3.skadnetwork",
+                "f38h382jlk.skadnetwork", "hs6bdukanm.skadnetwork", "9rd848q2bz.skadnetwork", "prcb7njmu6.skadnetwork",
+                "52fl2v3hgk.skadnetwork", "m8dbw4sv7c.skadnetwork", "v9wttpbfk9.skadnetwork", "n38lu8286q.skadnetwork",
+                "cstr6suwn9.skadnetwork", "w9q455wk68.skadnetwork", "wzmmz9fp6w.skadnetwork", "su67r6k2v3.skadnetwork",
+                "yclnxrl5pm.skadnetwork", "4468km3ulz.skadnetwork", "v79kvwwj4g.skadnetwork", "t38b2kh725.skadnetwork",
+                "7ug5zh24hu.skadnetwork", "5lm9lj6jb7.skadnetwork", "zmvfpc5aq8.skadnetwork", "kbd757ywx3.skadnetwork",
+                "275upjj5gd.skadnetwork", "9t245vhmpl.skadnetwork", "44jx6755aq.skadnetwork", "tl55sbb4fm.skadnetwork",
+                "2u9pt9hc89.skadnetwork", "8s468mfl3y.skadnetwork", "glqzh8vgby.skadnetwork", "av6w8kgt66.skadnetwork",
+                "klf5c3l5u5.skadnetwork", "ppxm28t8ap.skadnetwork", "424m5254lk.skadnetwork", "f73kdq92p3.skadnetwork",
+                "44n7hlldy6.skadnetwork", "ecpz2srf59.skadnetwork", "4dzt52r2t5.skadnetwork", "bvpn9ufa9b.skadnetwork",
+                "3rd42ekr43.skadnetwork"
+            };
+            AddSkAdNetwork(plist, networks.ToArray());
+        }
+
+        private static void AddSkAdNetwork(PlistDocument plist, params string[] networks) {
+            var items = plist.root["SKAdNetworkItems"]?.AsArray() ?? plist.root.CreateArray("SKAdNetworkItems");
+            foreach (var network in networks) {
+                items.AddDict().SetString("SKAdNetworkIdentifier", network);
             }
-            if (settings.IsAdMobMediationEnabled ||
-                settings.IsFacebookAdsEnabled ||
-                settings.IsIronSourceMediationEnabled) {
-                var items = plist.root["SKAdNetworkItems"]?.AsArray() ?? plist.root.CreateArray("SKAdNetworkItems");
-                items.AddDict().SetString("SKAdNetworkIdentifier", "v9wttpbfk9.skadnetwork");
-                items.AddDict().SetString("SKAdNetworkIdentifier", "n38lu8286q.skadnetwork");
-            }
-            if (settings.IsAdMobMediationEnabled ||
-                settings.IsIronSourceMediationEnabled ||
-                settings.IsUnityAdsEnabled) {
-                var items = plist.root["SKAdNetworkItems"]?.AsArray() ?? plist.root.CreateArray("SKAdNetworkItems");
-                items.AddDict().SetString("SKAdNetworkIdentifier", "4DZT52R2T5.skadnetwork");
-                items.AddDict().SetString("SKAdNetworkIdentifier", "bvpn9ufa9b.skadnetwork");
-            }
-            if (settings.IsAdMobMediationEnabled ||
-                settings.IsIronSourceEnabled) {
-                // IronSource.
-                var items = plist.root["SKAdNetworkItems"]?.AsArray() ?? plist.root.CreateArray("SKAdNetworkItems");
-                items.AddDict().SetString("SKAdNetworkIdentifier", "SU67R6K2V3.skadnetwork");
-            }
-            plist.WriteToFile(plistPath);
         }
     }
 }

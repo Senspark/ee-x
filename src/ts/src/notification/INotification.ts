555dfe737600526dd7fc0758120cf025332b4da2
eps <enrevol@gmail.com>
2021-02-23 11:03:01 +0700

    Fix to extends IPlugin (ts).

--- src/ts/src/notification/INotification.ts
+++ src/ts/src/notification/INotification.ts
@@ -1,6 +1,7 @@
+import { IPlugin } from "../core";
 import { NotificationBuilder } from "./NotificationBuilder";
 
-export interface INotification {
+export interface INotification extends IPlugin {
     schedule(builder: NotificationBuilder): void;
     unschedule(tag: number): void;
     clearAll(): void;

d4525c3f4a1ceb97c3809cb7f30fb3e189da85ec
eps <enrevol@gmail.com>
2021-02-26 17:30:16 +0700

    Fix init firebase parameter.

--- src/unity/Runtime/Services/Internal/FirebaseParameter.cs
+++ src/unity/Runtime/Services/Internal/FirebaseParameter.cs
@@ -1,44 +1,42 @@
 using System;
+using System.Reflection;
 
 namespace EE.Internal {
     internal class FirebaseParameter {
         private const string TypeName = "Firebase.Analytics.Parameter, Firebase.Analytics";
         private static readonly Type _type = Type.GetType(TypeName);
 
-        private static readonly ObjectActivator<object> _constructor0 =
-            ObjectActivatorUtils.GetActivator<object>(
-                _type.GetConstructor(new[] {
-                    typeof(string),
-                    typeof(string)
-                }));
+        private static readonly ConstructorInfo _constructor0 =
+            _type.GetConstructor(new[] {
+                typeof(string),
+                typeof(string)
+            });
 
-        private static readonly ObjectActivator<object> _constructor1 =
-            ObjectActivatorUtils.GetActivator<object>(
-                _type.GetConstructor(new[] {
-                    typeof(string),
-                    typeof(long)
-                }));
+        private static readonly ConstructorInfo _constructor1 =
+            _type.GetConstructor(new[] {
+                typeof(string),
+                typeof(long)
+            });
 
-        private static readonly ObjectActivator<object> _constructor2 =
-            ObjectActivatorUtils.GetActivator<object>(
-                _type.GetConstructor(new[] {
-                    typeof(string),
-                    typeof(double)
-                }));
+        private static readonly ConstructorInfo _constructor2 =
+            _type.GetConstructor(new[] {
+                typeof(string),
+                typeof(double)
+            });
 
         public static Type InternalType { get; } = _type;
         public object Internal { get; }
 
         public FirebaseParameter(string name, string value) {
-            Internal = _constructor0(name, value);
+            Internal = _constructor0.Invoke(new object[] {name, value});
         }
 
         public FirebaseParameter(string name, long value) {
-            Internal = _constructor1(name, value);
+            Internal = _constructor1.Invoke(new object[] {name, value});
         }
 
         public FirebaseParameter(string name, double value) {
-            Internal = _constructor2(name, value);
+            Internal = _constructor2.Invoke(new object[] {name, value});
         }
     }
 }

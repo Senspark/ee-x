6d008d36e1da46f213b3097a4fb72c747a86ded4
hailh <hailh@senspark.com>
2022-05-03 15:25:37 +0700

    Fix set screen (firebase analytics).

--- src/unity/Runtime/Services/Internal/IFirebaseAnalyticsImpl.cs
+++ src/unity/Runtime/Services/Internal/IFirebaseAnalyticsImpl.cs
@@ -1,6 +1,6 @@
 namespace EE.Internal {
     internal interface IFirebaseAnalyticsImpl {
-        void SetCurrentScreen(string screenName, string screenClass);
+        void SetCurrentScreen(string screenName);
         void LogEvent(string name);
         void LogEvent(string name, (string, object)[] parameters);
     }

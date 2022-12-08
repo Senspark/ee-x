2c5c45ea3d9c4e0ecd9bb3f12f4dd088063d226a
hailh <hailh@senspark.com>
2022-12-09 02:22:39 +0700

    Format.

--- src/unity/Runtime/Services/Internal/FirebaseAnalyticsImpl.cs
+++ src/unity/Runtime/Services/Internal/FirebaseAnalyticsImpl.cs
@@ -22,18 +22,18 @@ namespace EE.Internal {
             _typeParameter = Type.GetType("Firebase.Analytics.Parameter, Firebase.Analytics");
             Assert.IsNotNull(_typeParameter);
 
-            _methodLogEvent = type.GetMethod("LogEvent", new[] {typeof(string)});
+            _methodLogEvent = type.GetMethod("LogEvent", new[] { typeof(string) });
             _methodLogEventParameters =
-                type.GetMethod("LogEvent", new[] {typeof(string), _typeParameter.MakeArrayType()});
+                type.GetMethod("LogEvent", new[] { typeof(string), _typeParameter.MakeArrayType() });
             Assert.IsNotNull(_methodLogEvent);
             Assert.IsNotNull(_methodLogEventParameters);
 
             _parameterScreenName = (string) type.GetProperty("ParameterScreenName")?.GetValue(null);
             Assert.IsNotNull(_parameterScreenName);
 
-            _constructorLong = _typeParameter.GetConstructor(new[] {typeof(string), typeof(long)});
-            _constructorDouble = _typeParameter.GetConstructor(new[] {typeof(string), typeof(double)});
-            _constructorString = _typeParameter.GetConstructor(new[] {typeof(string), typeof(string)});
+            _constructorLong = _typeParameter.GetConstructor(new[] { typeof(string), typeof(long) });
+            _constructorDouble = _typeParameter.GetConstructor(new[] { typeof(string), typeof(double) });
+            _constructorString = _typeParameter.GetConstructor(new[] { typeof(string), typeof(string) });
             Assert.IsNotNull(_constructorLong);
             Assert.IsNotNull(_constructorDouble);
             Assert.IsNotNull(_constructorString);
@@ -64,7 +64,7 @@ namespace EE.Internal {
         }
 
         private void LogEventInternal(string name) {
-            _methodLogEvent.Invoke(null, new object[] {name});
+            _methodLogEvent.Invoke(null, new object[] { name });
         }
 
         private void LogEventInternal(string name, (string, object)[] parameters) {
@@ -75,21 +75,21 @@ namespace EE.Internal {
                 var validatedParamName = LimitLength(paramName, 40);
                 var paramType = paramValue.GetType();
                 if (paramType == typeof(bool)) {
-                    param = _constructorLong.Invoke(new object[] {validatedParamName, (bool) paramValue ? 1 : 0});
+                    param = _constructorLong.Invoke(new object[] { validatedParamName, (bool) paramValue ? 1 : 0 });
                 } else if (paramType == typeof(int) || paramType == typeof(long)) {
-                    param = _constructorLong.Invoke(new[] {validatedParamName, paramValue});
+                    param = _constructorLong.Invoke(new[] { validatedParamName, paramValue });
                 } else if (paramType == typeof(float) || paramType == typeof(double)) {
-                    param = _constructorDouble.Invoke(new[] {validatedParamName, paramValue});
+                    param = _constructorDouble.Invoke(new[] { validatedParamName, paramValue });
                 } else if (paramType == typeof(string)) {
                     var validatedParamValue = LimitLength((string) paramValue, 100);
-                    param = _constructorString.Invoke(new object[] {validatedParamName, validatedParamValue});
+                    param = _constructorString.Invoke(new object[] { validatedParamName, validatedParamValue });
                 } else {
                     Assert.IsFalse(true);
                 }
                 firebaseParameters.SetValue(param, i);
             }
             var validatedName = LimitLength(name, 40);
-            _methodLogEventParameters.Invoke(null, new object[] {validatedName, firebaseParameters});
+            _methodLogEventParameters.Invoke(null, new object[] { validatedName, firebaseParameters });
         }
 
         private static string LimitLength(string value, int length) {

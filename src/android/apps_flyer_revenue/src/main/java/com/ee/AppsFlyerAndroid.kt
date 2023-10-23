b70e601199b5f27247e4de2878eaa6bba7c14053
nhannh <nhanc18@macs-MacBook-Pro.local>
2023-10-23 17:29:48 +0700

    update appsflyer callback

--- src/android/apps_flyer_revenue/src/main/java/com/ee/AppsFlyerAndroid.kt
+++ src/android/apps_flyer_revenue/src/main/java/com/ee/AppsFlyerAndroid.kt
@@ -82,7 +82,7 @@ class AppsFlyerIapResultListener : PurchaseClient.InAppPurchaseValidationResultL
 }
 
 class AppsFlyerSubscriptionResultListener : PurchaseClient.SubscriptionPurchaseValidationResultListener {
-    override fun onResponse(result: MutableMap<String, out SubscriptionValidationResult>?) {
+    override fun onResponse(result: Map<String, SubscriptionValidationResult>?) {
         result?.forEach { (k: String, v: SubscriptionValidationResult?) ->
             if (v.success) {
                 val productPurchase = v.subscriptionPurchase;

c33d1ec75cfdcaa387282f9e419506360bfa2d28
nhannh <nhanc18@macs-MacBook-Pro.local>
2023-11-23 16:00:18 +0700

    create fab button

--- src/android/command_receiver/src/main/java/com/ee/CommandReceiverBridge.kt
+++ src/android/command_receiver/src/main/java/com/ee/CommandReceiverBridge.kt
@@ -2,6 +2,8 @@ package com.ee
 
 import android.app.Activity
 import android.app.Application
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import com.senspark.unity_cmd_receiver.UnityCmdReceiver
 
 class CommandReceiverBridge(
@@ -16,11 +18,28 @@ class CommandReceiverBridge(
         private const val kOnMessageReceived = "${kPrefix}OnMessageReceived"
     }
 
+    private lateinit var fab: com.ee.cheat.Fab
+
     init {
         _logger.info("$kTag: constructor begin: application = $_application activity = $_activity")
         registerHandlers()
         UnityCmdReceiver.callback = { message ->
             onMessageReceived(message)
+        }
+        if(_activity != null) {
+            val rootView = _activity!!.window.decorView.rootView
+            val listener = ViewTreeObserver.OnGlobalLayoutListener {
+                if (!::fab.isInitialized) {
+                    val activity = _activity!!
+                    val density = activity.resources.displayMetrics.density
+                    val sizeInPx = (48 * density).toInt()
+                    fab = com.ee.cheat.Fab(activity, rootView)
+                    fab.layoutParams = ViewGroup.LayoutParams(sizeInPx, sizeInPx)
+                    val viewGroup = activity.window.decorView as ViewGroup
+                    viewGroup.addView(fab)
+                }
+            }
+            rootView.viewTreeObserver.addOnGlobalLayoutListener(listener)
         }
         _logger.info("$kTag: constructor end")
     }
---
+++ src/android/command_receiver/src/main/java/com/ee/cheat/Fab.kt
@@ -0,0 +1,115 @@
+package com.ee.cheat
+
+import android.app.Activity
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlin.math.abs
+
+class Fab(
+    activity: Activity,
+    parentView: View
+) :
+    FloatingActionButton(activity),
+    View.OnTouchListener {
+
+    companion object {
+        private const val CLICK_DRAG_TOLERANCE =
+            10f // Often, there will be a slight, unintentional, drag when the user taps the FAB, so we need to account for this.
+    }
+
+    private var downRawX = 0f
+    private var downRawY: Float = 0f
+    private var dX = 0f
+    private var dY: Float = 0f
+
+    init {
+        val orientation = activity.resources.configuration.orientation
+        if (orientation == android.content.res.Configuration.ORIENTATION_LANDSCAPE) {
+            x = parentView.width / 2f
+            y = 0f
+        } else {
+            x = 0f
+            y = parentView.height / 2f
+        }
+        size = SIZE_MINI
+        setOnTouchListener(this)
+    }
+
+    override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+        return when (motionEvent.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return onTouchStart(view, motionEvent)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                return onTouchMove(view, motionEvent)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                return onTouchEnd(view, motionEvent)
+            }
+
+            else -> {
+                super.onTouchEvent(motionEvent)
+            }
+        }
+    }
+
+    private fun onTouchStart(
+        view: View,
+        motionEvent: MotionEvent
+    ): Boolean {
+        downRawX = motionEvent.rawX
+        downRawY = motionEvent.rawY
+        dX = view.x - downRawX
+        dY = view.y - downRawY
+        return true // Consumed
+    }
+
+    private fun onTouchEnd(
+        view: View,
+        motionEvent: MotionEvent
+    ): Boolean {
+        val upRawX = motionEvent.rawX
+        val upRawY = motionEvent.rawY
+        val upDX = upRawX - downRawX
+        val upDY: Float = upRawY - downRawY
+        if (abs(upDX) < CLICK_DRAG_TOLERANCE && abs(upDY) < CLICK_DRAG_TOLERANCE) { // A click
+            log("onTouchEnd: click")
+            return performClick()
+        } else { // A drag
+            return true // Consumed
+        }
+    }
+
+    private fun onTouchMove(
+        view: View,
+        motionEvent: MotionEvent
+    ): Boolean {
+        val viewWidth = view.width
+        val viewHeight = view.height
+        val viewParent = view.parent as View
+        val parentWidth = viewParent.width
+        val parentHeight = viewParent.height
+
+        var newX = motionEvent.rawX + dX
+        var newY = motionEvent.rawY + dY
+        newX = clamp(newX, 0f, (parentWidth - viewWidth).toFloat())
+        newY = clamp(newY, 0f, (parentHeight - viewHeight).toFloat())
+
+        view.x = newX
+        view.y = newY
+
+        return true // Consumed
+    }
+
+    private fun clamp(value: Float, min: Float, max: Float): Float {
+        return value.coerceAtLeast(min).coerceAtMost(max)
+    }
+
+    private fun log(message: String) {
+        Log.d("nhanc19", message)
+    }
+}
